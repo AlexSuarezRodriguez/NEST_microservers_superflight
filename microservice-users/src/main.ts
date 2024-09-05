4dbe72e31350e1ee4a8c98dad430ccc6a4e2b86a
AlexSuarezR <alex.suarez@hotmail.com>
2024-09-04 20:15:04 -0500

    feat: fix conection RabbitMQ

--- microservice-users/src/main.ts
+++ microservice-users/src/main.ts
@@ -4,13 +4,14 @@ import { Transport } from '@nestjs/microservices';
 import { RabbitMQ } from './common/constants';
 
 async function bootstrap() {
-  const app = await NestFactory.createMicroservice(AppModule,{
-    Transport: Transport.RMQ,
-    options:{
-      urls:[process.env.AMQP_URL],
-      queue: RabbitMQ.UserQueue
-    }
+  const app = await NestFactory.createMicroservice(AppModule, {
+    transport: Transport.RMQ,
+    options: {
+      urls: [process.env.AMQP_URL],
+      queue: RabbitMQ.UserQueue,
+    },
   });
   await app.listen();
+  console.log(`Microservice Users is listening`);
 }
 bootstrap();
