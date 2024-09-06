b592a88782c7186c46eefb5a2eaace2f0684b51d
AlexSuarezR <alex.suarez@hotmail.com>
2024-09-05 20:48:34 -0500

    feat: config module auth

--- microservice-users/src/user/user.controller.ts
+++ microservice-users/src/user/user.controller.ts
@@ -6,31 +6,43 @@ import { UserMSG } from 'src/common/constants';
 
 @Controller() 
 export class UserController {
-  constructor(private readonly usersService: UserService) {}
+  constructor(private readonly userService: UserService) {}
 
   @MessagePattern(UserMSG.CREATE)
   create(@Payload() userDTO: UserDTO) {
-    return this.usersService.create({ userDTO });
+    return this.userService.create({ userDTO });
   }
 
   @MessagePattern(UserMSG.FIND_ALL)
   findAll() {
-    return this.usersService.findAll();
+    return this.userService.findAll();
   }
 
   @MessagePattern(UserMSG.FIND_ONE)
   findOne(@Payload('id') id: string) {
-    return this.usersService.findOne({ id });
+    return this.userService.findOne({ id });
   }
 
   @MessagePattern(UserMSG.UPDATE)
   updateById(@Payload() payload: any) {
     const{id, userDTO} = payload
-    return this.usersService.updateById({ id, userDTO });
+    return this.userService.updateById({ id, userDTO });
   }
 
   @MessagePattern(UserMSG.DELETE)
   deleteById(@Payload('id') id: string) {
-    return this.usersService.deleteById({ id });
+    return this.userService.deleteById({ id });
+  }
+
+  @MessagePattern(UserMSG.VALID_USER)
+  async validateUser(@Payload() payload:any): Promise<any> {
+    const { username, password } = payload;
+    const user = await this.userService.findByUsername({ username });
+    const isValidPassword = await this.userService.checkPassword({ password, passwordRegister: user.password });
+    if (user && isValidPassword) {
+      return user;
+    }
+
+    return null;
   }
 }
